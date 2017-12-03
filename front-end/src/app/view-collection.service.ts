a700b166a5845e33461cf14113ad76a86f2168f7
Stuart Wilcox <stuart_wilcox@outlook.com>
2017-12-03 15:00:33 -0500

    Can add images to collection

--- front-end/src/app/view-collection.service.ts
+++ front-end/src/app/view-collection.service.ts
@@ -15,4 +15,21 @@ export class ViewCollectionService {
     return this.http.get(`/api/collections/${id}`, {headers:headers});
   }
 
+  getUserCollections(token, user_id){
+    let headers = new HttpHeaders();
+    headers = headers.append("Cookie", "token="+token);
+    headers = headers.append("Content-Type", "application/json");
+    headers = headers.append("Accept", "*/*");
+
+    return this.http.get(`/api/${user_id}/collections`, {headers:headers});
+  }
+
+  addImageToCollection(token, collection_id, image_id){
+    let headers = new HttpHeaders();
+    headers = headers.append("Cookie", "token="+token);
+    headers = headers.append("Content-Type", "application/json");
+    headers = headers.append("Accept", "*/*");
+
+    return this.http.post(`/api/collections/${collection_id}`,{image_id:image_id}, {headers:headers});
+  }
 }
--- front-end/src/app/view-image/view-image.component.ts
+++ front-end/src/app/view-image/view-image.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DashboardService } from '../dashboard.service';
 import { ViewImageService } from '../view-image.service';
+import { ViewCollectionService } from '../view-collection.service';
 
 @Component({
   selector: 'app-view-image',
   templateUrl: './view-image.component.html',
   styleUrls: ['./view-image.component.css'],
-  providers:[DashboardService, ViewImageService]
+  providers:[DashboardService, ViewImageService, ViewCollectionService]
 })
 export class ViewImageComponent implements OnInit {
-  loading:boolean;
+  job:any;
   asset_id: string;
   user:object;
   nasa_object: object;//two components: asset and metadata
+  collections: object;
 
-  constructor(private dashboardService: DashboardService, private router:Router, private viewImageService:ViewImageService) {
+  constructor(private dashboardService: DashboardService, private router:Router, private viewImageService:ViewImageService, private viewCollectionService:ViewCollectionService) {
     this.asset_id = this.router.url.split("/")[2];
-    this.loading = true;
     this.user = null;
+    this.job = [];
+    this.collections = null;
   }
 
   getCookie(cname) {
@@ -38,54 +41,65 @@ export class ViewImageComponent implements OnInit {
   }
 
   ngOnInit() {
-    let job1=true, job2=true, job3=true;
+    this.job.push(0);this.job.push(4)//job=[0, 3];//the first is the number of complete jobs. The second is the number of jobs. When a job is done, increment the first number and compare it to the second
     let token = this.getCookie("token");
 
+    //token only exists if logged in
+
     if(token){
+
+      //get the user data
       this.dashboardService.getUserData(token).subscribe(
         data => {
-            job1=false;
-            this.loading = job1||job2||job3;
+            this.job[0]++;
             this.user=data;
+
+            //populate the collections option at the bottom
+            this.viewCollectionService.getUserCollections(token, this.user['_id']).subscribe(
+              data=>{
+                this.job[0]++;
+                this.collections=data;
+              },
+              err=>{
+                console.log(err);
+              }
+            );
         },
         err => {
-          this.loading=false;
               this.user={name:"An error occurred."};
               console.log("err", err);
           }
       );
     }
     else{
-      this.user={name:"Guest"};
-      job1=false;
-      this.loading = job1||job2||job3;
+      //TODO: redirect to search
     }
 
+    //make sure there is really an assetId
+
     if(this.asset_id){
-      this.nasa_object = {};
+      this.nasa_object = {};//object to hold the metadata and asset
+
+      //get the asset
       this.viewImageService.getAsset(this.asset_id).subscribe(
         data=>{
-          job2=false;
-          this.loading = job1||job2||job3;
-          console.log(data);
-          this.nasa_object.asset = data;
+          this.job[0]++;
+          this.nasa_object['asset'] = data;
         },
         err=>{
           console.log(err);
-          job2=false;
-          this.loading = job1||job2||job3;
         }
       );
-      let metaDataUrl = "";
+
+      //get the metadata. This takes two steps since the first one only fetches to url to find the metadata at
       this.viewImageService.getMetadataUrl(this.asset_id).subscribe(
         data=>{
-          metaDataUrl = data['location'];
-          this.viewImageService.getMetadata(metaDataUrl).subscribe(
+
+          //get the actual metadata
+          this.viewImageService.getMetadata(data['location']).subscribe(
             data=>{
-              job3=false;
-              this.loading = job1||job2||job3;
-              console.log(data);
-              this.nasa_object.metadata = data;
+              this.job[0]++;
+              this.nasa_object['metadata'] = data;
             },
             err=>{
               console.log(err);
@@ -96,10 +110,39 @@ export class ViewImageComponent implements OnInit {
           console.log(err);
         }
       );
-    }else{
-      console.log("Shouldnt be here")
+    }
+    else{
+      //the is no asset_id but that should never happen.
+      //should redirect to somewhere
+      console.log("Shouldnt be here");
+    }
+  }
+
+
+  addToCollection(event, collectionId){
+    event.preventDefault();
+
+    if(collectionId=="null"){
+      //null option
+      return;
     }
 
+    console.log(collectionId);
+    console.log(this.nasa_object['asset'].collection.items[0].href);
+
+    let image_id = this.nasa_object['asset'].collection.items[0].href;
+
+
+    //add the image (url=image_id) to the collection (collectionId)
+    this.viewCollectionService.addImageToCollection(this.getCookie("token"), collectionId, image_id).subscribe(
+      data=>{
+        //TODO: finish this
+        console.log(data);
+      },
+      err=>{
+        console.log(err);
+      }
+    );
 
   }
 
